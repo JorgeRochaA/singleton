fcf939c62be2b63e2cde8dce82c5f58bd02940b8
Jorge Rocha <137959832+JorgeRochaFreshConsulting@users.noreply.github.com>
2023-12-23 01:19:53 -0600

    test

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@ import Navbar from "./shared/components/Navbar.vue";
 </script>
 
 <template>
+  <div class="app">
     <router-view />
+  </div>
 </template>
 
 <style scoped>
@@ -11,5 +13,9 @@ import Navbar from "./shared/components/Navbar.vue";
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+}
+.app {
+  min-height: 100vh;
+  width: 100vw;
 }
 </style>
